83f673e3466e8e3dddb9faaa9715d3095d3bc5c4
tamasalexa <69505757+tamasalexa@users.noreply.github.com>
2022-06-22 13:24:10 +0300

    22.06.2022

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 import * as THREE from './three125/three.js';
-import { MTLLoader } from './three125/MTLLoader.js';    
+import { MTLLoader } from './three125/MTLLoader.js';
 import { OBJLoader } from './three125/OBJLoader.js';
 import { RGBELoader } from './three125/RGBELoader.js';
+import { TransformControls } from './three125/TransformControls.js'
 
 import { LoadingBar } from './LoadingBar.js';
 
@@ -29,7 +30,12 @@ class App {
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         this.renderer.outputEncoding = THREE.sRGBEncoding;
         container.appendChild(this.renderer.domElement);
-        //this.setEnvironment();
+      
+        this.transformControls = new TransformControls(this.camera, this.renderer.domElement);
+        this.transformControls.addEventListener('change', this.render.bind(this));
+        this.transformControls.addEventListener('dragging-changed', (event) => {
+            this.orbitControls.enabled = !event.value;
+        });
 
         this.reticle = new THREE.Mesh(
             new THREE.RingBufferGeometry(0.15, 0.2, 32).rotateX(- Math.PI / 2),
@@ -69,22 +75,29 @@ class App {
         this.hitTestSource = null;
 
         function onSelect() {
+            console.log("!!!! -0- onSelect");
             console.log("!!!! -0- onSelect", self.obj3D);
             if (self.obj3D === undefined) return;
             console.log("!!!! -1- onSelect");
             if (self.reticle.visible) {
                 self.obj3D.position.setFromMatrixPosition(self.reticle.matrix);
                 self.obj3D.visible = true;
+                self.scene.add(self.transformControls);
                 console.log("!!!! -2- onSelect", self.obj3D);
+
             }
         }
 
         this.controller = this.renderer.xr.getController(0);
+
         console.log("this.controller", this.controller);
         console.log("this.renderer.xr", this.renderer.xr);
-
-        this.controller.addEventListener('select', onSelect);
+        
+        this.controller.addEventListener('select', onSelect);        
         this.controller.addEventListener('touch', () => { let date = new Date(); console.log(`click - ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} `); });
+
+        console.log("this.controller", this.controller);
+        console.log("this.renderer.xr", this.renderer.xr);
 
         this.scene.add(this.controller);
     }
@@ -95,107 +108,7 @@ class App {
         this.renderer.setSize(window.innerWidth, window.innerHeight);
     }
 
-    //setEnvironment(){
-    //    const loader = new RGBELoader().setDataType( THREE.UnsignedByteType );
-    //    const pmremGenerator = new THREE.PMREMGenerator( this.renderer );
-    //    pmremGenerator.compileEquirectangularShader();
-
-    //    const self = this;
-
-    //    loader.load( '../../assets/hdr/venice_sunset_1k.hdr', ( texture ) => {
-    //      const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
-    //      pmremGenerator.dispose();
-
-    //      self.scene.environment = envMap;
-
-    //    }, undefined, (err)=>{
-    //        console.error( 'An error occurred setting the environment');
-    //    } );
-    //}
-
-    //showObj3D_orig(id){
-    //       this.initAR();
-
-    //	const loader = new GLTFLoader( ).setPath(this.assetsPath);
-    //       const self = this;
-
-    //       this.loadingBar.visible = true;
-
-    //	// Load a glTF resource
-    //	loader.load(
-    //		// resource URL
-    //		`obj3D${id}.glb`,
-    //		// called when the resource is loaded
-    //		function ( gltf ) {
-
-    //			self.scene.add( gltf.scene );
-    //               self.obj3D = gltf.scene;
-
-    //               self.obj3D.visible = false; 
-
-    //               self.loadingBar.visible = false;
-
-    //               self.renderer.setAnimationLoop( self.render.bind(self) );
-    //		},
-    //		// called while loading is progressing
-    //		function ( xhr ) {
-
-    //			self.loadingBar.progress = (xhr.loaded / xhr.total);
-
-    //		},
-    //		// called when loading has errors
-    //		function ( error ) {
-
-    //			console.log( 'An error happened' );
-
-    //		}
-    //       );
-    //}			
-
-    showObj3D1(objURL) {
-
-        this.initAR();
-        const self = this;
-        this.loadingBar.visible = true;
-        const loader = new OBJLoader();
-        loader.load(
-            // resource URL
-            objURL,
-
-            // onLoad callback
-            // Here the loaded data is assumed to be an object
-            function (obj) {
-                // Add the loaded object to the scene
-                self.scene.add(obj);
-
-                self.obj3D = obj;
-
-                self.obj3D.visible = false;
-
-                self.loadingBar.visible = false;
-
-                self.renderer.setAnimationLoop(self.render.bind(self));
-
-
-
-            },
-
-            // onProgress callback
-            function (xhr) {
-                console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-                self.loadingBar.progress = (xhr.loaded / xhr.total);
-            },
-
-            // onError callback
-            function (error) {
-                console.error('An error happened', error);
-            }
-        );
-
-
-
-    }
-
+    
     showObj3D(texturesURL, objURL, objName) {
         if (texturesURL === null) {
             texturesURL = objURL;
@@ -228,48 +141,22 @@ class App {
                     if (child.isMesh) {
                         //child.scale.x = child.scale.y = child.scale.z = scaleVal;
                     }
-                });
-
-                //if (obj.type == "Group") {
-
-                //    if (isStair || obj.children.length > 1) {
-                        //if (isStair) {
-                        //obj.rotation.x = (3 * Math.PI) / 2;
-                        //}
-
-                        //let box = new THREE.Box3().setFromObject(object);
-                        //let width = (Math.abs(box.max.x) + Math.abs(box.min.x));
-                        //let height = (Math.abs(box.max.y) + Math.abs(box.min.y));
-                        //let depth = (Math.abs(box.max.z) + Math.abs(box.min.z));
-                        //console.log("width = " + width);
-                        //console.log("height = " + height);
-                        //console.log("depth = " + depth);
-
-                        //let boxGeometry = new THREE.BoxGeometry(width, height, depth);
-                        //geometry = new THREE.Geometry();
-                        //geometry.vertices = boxGeometry.vertices;
-                        //geometry.faces = boxGeometry.faces;
-                        //geometry.faceVertexUvs = boxGeometry.faceVertexUvs;
-
-                        //material = new THREE.MeshLambertMaterial({ color: 0xa1a1cf, opacity: 0, transparent: true });
-                  //  } else if (obj.children.length == 1 && obj.children[0].isMesh) {
-                        //let child = object.children[0];
-                        //child.geometry.scale(scaleVal, scaleVal, scaleVal);
-                        //geometry = child.geometry;
-                        //material = child.material;
-                   // }
-                //}
-               
+                });               
 
                 self.scene.add(obj);
-
+                console.log("0 showObj3D");
                 self.obj3D = obj;
-
+                console.log("1 showObj3D");
                 self.obj3D.visible = false;
-
+                console.log("2 showObj3D");
                 self.loadingBar.visible = false;
-
+                console.log("3 showObj3D");
                 self.renderer.setAnimationLoop(self.render.bind(self));
+                console.log("4 showObj3D");
+                self.transformControls.attach(self.obj3D);
+                self.transformControls.setMode("rotate");
+                //self.scene.add(self.transformControls);
+                console.log("5 showObj3D");
               
                 //if (isStair) {
                 //    //center the object
